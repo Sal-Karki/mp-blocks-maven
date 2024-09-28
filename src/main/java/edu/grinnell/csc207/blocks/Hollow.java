9b834683907d463201c20d1f911b31a9fc60c2e6
class5thsept <nunesleo@grinnell.edu>
2024-09-28 16:13:27 -0500

    Part 9: New block Hollow

---
+++ src/main/java/edu/grinnell/csc207/blocks/Hollow.java
@@ -0,0 +1,89 @@
+package edu.grinnell.csc207.blocks;
+
+/**
+ * A hollow version of an ASCII block.
+ *
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here
+ */
+public class Hollow implements AsciiBlock {
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The original block.
+   */
+  AsciiBlock block;
+
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new block with the specified contents.
+   *
+   * @param original
+   *   The original block.
+   */
+  public Hollow(AsciiBlock original) {
+    this.block = original;
+  } // Trimmed(AsciiBlock, HAlignment, VAlignment, int, int)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Get one row from the block.
+   *
+   * @param i the number of the row
+   *
+   * @return row i.
+   *
+   * @exception Exception
+   *   If the row is invalid.
+   */
+  public String row(int i) throws Exception {
+    String row = new String("");
+    if (i == 0){
+      row = block.row(i);
+    } else if (i == block.height()) {
+      row = block.row(i);
+    } else {
+      row = block.row(i).substring(0).concat(" ".repeat(block.width() - 2)).concat(block.row(i).substring(block.width() - 1));
+    }
+    return row;
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   *
+   * @return the number of rows
+   */
+  public int height() {
+    return block.height();
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   *
+   * @return the number of columns
+   */
+  public int width() {
+    return block.width();
+  } // width()
+
+  /**
+   * Determine if another block is structurally equivalent to this block.
+   *
+   * @param other
+   *   The block to compare to this block.
+   *
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
+   */
+  public boolean eqv(AsciiBlock other) {
+    return false;       // STUB
+  } // eqv(AsciiBlock)
+} // class Trimmed
